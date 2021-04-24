2b8c753994bbdbd5df24ca81ecb581f6b259d754
Yuriy Koziy <yukoziy@gmail.com>
2021-04-24 19:55:54 -0400

    Added dom utility module to store DOM related functions

---
+++ src/modules/util/dom.js
@@ -0,0 +1,23 @@
+const createElement = (type, id, classList, attributes, txt) => {
+    const element = document.createElement(type);
+    if (id) {
+        element.id = id;
+    }
+
+    if (classList) {
+        element.classList.add(classList.split(" "));
+    }
+
+    if (attributes) {
+        //TODO
+    }
+
+    if (txt) {
+        const text = document.createTextNode(txt);
+        element.appendChild(text);
+    }
+
+    return element;
+}
+
+export { createElement };
