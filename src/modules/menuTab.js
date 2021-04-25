037a5214f055209770857be9fcd4967c25b3f835
Yuriy Koziy <yukoziy@gmail.com>
2021-04-25 19:35:08 -0400

    Basic layout for the menu page is done

--- src/modules/menuTab.js
+++ src/modules/menuTab.js
@@ -1,13 +1,36 @@
 import { createElement } from './util/dom';
 
+import '../styles/menu.scss';
+
 const generate = (container) => {
     container.appendChild(createMenuDiv());
 }
 
 const createMenuDiv = () => {
-    const pElement = createElement("p", null, null, null, "Menu goes here.....");
+    const menuDiv = createElement("div", "menu");
 
-    return pElement;
+    for (let i = 0; i <= 8; i++) {
+        menuDiv.appendChild(createMenuItem(i+1));
+    }
+
+    return menuDiv;
+}
+
+const createMenuItem = (title) => {
+    const menuItem = createElement("div", null, "menu-item");
+
+    const titleElement = createElement("h1", null, "menu-item-title", null, `Menu Item ${title}`);
+    const imgElement = createElement("div", null, "menu-item-img");
+
+
+    const txt = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.';
+    const textElement = createElement("p", null, "menu-item-text", null, txt+txt);
+
+    menuItem.appendChild(titleElement);
+    menuItem.appendChild(imgElement);
+    menuItem.appendChild(textElement);
+
+    return menuItem;
 }
 
 export default generate;
